58a7a07c3742d2ec196943ad1e6d60bbf7a13dbb
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-03-06 18:16:10 -0500

    created the firstNumber, secondNumber, and operator variables

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const operatorButton = document.querySelectorAll(".operator")
 const equalButton = document.querySelector(".equal")
 const clearButton = document.querySelector(".clear")
 const display = document.querySelector(".display")
+
+let firstNumber = ""
+let secondNumber = ""
+let operator = ""
