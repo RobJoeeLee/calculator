201048ae8b0b8a46d933c21b2e6bd21f47e4d165
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-03-06 14:59:35 -0500

    brought my html elements into js

---
+++ script.js
@@ -0,0 +1,5 @@
+const numberButton = document.querySelectorAll(".number")
+const operatorButton = document.querySelectorAll(".operator")
+const equalButton = document.querySelector(".equal")
+const clearButton = document.querySelector(".clear")
+const display = document.querySelector(".display")
