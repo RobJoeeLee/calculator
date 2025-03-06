d9ee75b313618059aec46e2a7efe0fba3909a02a
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-03-06 18:19:01 -0500

    created the add, subract, mulitply, and divide functions

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const display = document.querySelector(".display")
 let firstNumber = ""
 let secondNumber = ""
 let operator = ""
+
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    if (b === "0"){
+        return "Error: Can't divide by 0!"
+    }
+    return a / b
+}
+
